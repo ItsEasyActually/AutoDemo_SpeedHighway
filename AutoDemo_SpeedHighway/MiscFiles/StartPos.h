a9fb65e8de3051748cddc7d822559506f079b972
ItsEasyActually <darkspines35@gmail.com>
2019-02-07 23:31:13 -0500

    Initial Add for a lot of files and organization from base build (not on git). Currently will not build as much of the data is missing still. Still in progress.

---
+++ AutoDemo_SpeedHighway/MiscFiles/StartPos.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SADXModLoader.h>
+
+StartPosition sonicSpeedHighway00 = { LevelIDs_SpeedHighway, 0, { -673.0, -10.0, 5.0 }, 0};
+StartPosition sonicSpeedHighway01 = { LevelIDs_SpeedHighway, 1, { -50.0, 42.0, 0.0 }, 0 };
+StartPosition sonicSpeedHighway02 = { LevelIDs_SpeedHighway, 2, { 39.0, 27.0, 215.0 }, 0 };
+
+StartPosition milesSpeedHighway00 = { LevelIDs_SpeedHighway, 0, { -673.0, -10.0, 5.0 }, 0 };
+
+StartPosition knucklesSpeedHighway02 = { LevelIDs_SpeedHighway, 2, { -230.0, 155.0, -1875.0 }, 0x4000 };
